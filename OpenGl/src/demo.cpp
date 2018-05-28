db0028b2cf5d4b66fbe67d0da803b642d26d8c2a
Rahul Gautam <arr.gee23@gmail.com>
2018-05-29 01:32:32 +0530

    opengl initial

---
+++ OpenGl/src/demo.cpp
@@ -0,0 +1,11 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <cstdio>
+#include <iostream>
+#include "/usr/include/GL/glut.h"
+
+using namespace std;
+int main2(){
+	std::cout<<"ss";
+	return 0;
+}
